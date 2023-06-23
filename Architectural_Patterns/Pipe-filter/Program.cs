9c676c069ad4f3cad0755d17c90d760e2b7b6c2a
Bohdan Shaposhnyk <bohdan.shaposhnyk@extendaretail.com>
2023-06-23 18:58:04 +0300

    Add Pipe-filter pattern

---
+++ Architectural_Patterns/Pipe-filter/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PipeFilterPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pipe-Filter Pattern Example");
+
+            // Create a list of numbers
+            List<int> numbers = Enumerable.Range(1, 10).ToList();
+
+            // Create filters
+            var filter1 = new MultiplyFilter(2);
+            var filter2 = new SubtractFilter(5);
+
+            // Create the pipeline
+            var pipeline = new Pipeline<int>();
+
+            // Add filters to the pipeline
+            pipeline.Register(filter1);
+            pipeline.Register(filter2);
+
+            // Process the numbers through the pipeline
+            List<int> results = pipeline.Process(numbers);
+
+            // Print the results
+            Console.WriteLine("Results:");
+            foreach (int result in results)
+            {
+                Console.WriteLine(result);
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+
+    // Filter interface
+    interface IFilter<T>
+    {
+        T Process(T input);
+    }
+
+    // Multiply filter
+    class MultiplyFilter : IFilter<int>
+    {
+        private int factor;
+
+        public MultiplyFilter(int factor)
+        {
+            this.factor = factor;
+        }
+
+        public int Process(int input)
+        {
+            int result = input * factor;
+            Console.WriteLine($"MultiplyFilter: {input} * {factor} = {result}");
+            return result;
+        }
+    }
+
+    // Subtract filter
+    class SubtractFilter : IFilter<int>
+    {
+        private int value;
+
+        public SubtractFilter(int value)
+        {
+            this.value = value;
+        }
+
+        public int Process(int input)
+        {
+            int result = input - value;
+            Console.WriteLine($"SubtractFilter: {input} - {value} = {result}");
+            return result;
+        }
+    }
+
+    // Pipeline class
+    class Pipeline<T>
+    {
+        private List<IFilter<T>> filters;
+
+        public Pipeline()
+        {
+            filters = new List<IFilter<T>>();
+        }
+
+        public void Register(IFilter<T> filter)
+        {
+            filters.Add(filter);
+        }
+
+        public List<T> Process(List<T> input)
+        {
+            List<T> output = new List<T>(input);
+
+            foreach (IFilter<T> filter in filters)
+            {
+                output = output.Select(filter.Process).ToList();
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Architectural_Patterns/Pipe-filter/Tests.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace PipeFilterPattern.Tests
+{
+    public class PipeFilterTests
+    {
+        [Fact]
+        public void TestMultiplyFilter()
+        {
+            // Arrange
+            var filter = new MultiplyFilter(3);
+            int input = 5;
+
+            // Act
+            int result = filter.Process(input);
+
+            // Assert
+            Assert.Equal(15, result);
+        }
+
+        [Fact]
+        public void TestSubtractFilter()
+        {
+            // Arrange
+            var filter = new SubtractFilter(5);
+            int input = 12;
+
+            // Act
+            int result = filter.Process(input);
+
+            // Assert
+            Assert.Equal(7, result);
+        }
+
+        [Fact]
+        public void TestEmptyPipeline()
+        {
+            // Arrange
+            var pipeline = new Pipeline<int>();
+            var numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+            // Act
+            List<int> results = pipeline.Process(numbers);
+
+            // Assert
+            Assert.Equal(numbers, results);
+        }
+
+        [Fact]
+        public void TestSingleFilterPipeline()
+        {
+            // Arrange
+            var pipeline = new Pipeline<int>();
+            var filter = new MultiplyFilter(2);
+            pipeline.Register(filter);
+            var numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+            // Act
+            List<int> results = pipeline.Process(numbers);
+
+            // Assert
+            Assert.Equal(new List<int> { 2, 4, 6, 8, 10 }, results);
+        }
+
+        [Fact]
+        public void TestMultipleFiltersPipeline()
+        {
+            // Arrange
+            var pipeline = new Pipeline<int>();
+            var filter1 = new MultiplyFilter(2);
+            var filter2 = new SubtractFilter(3);
+            pipeline.Register(filter1);
+            pipeline.Register(filter2);
+            var numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+            // Act
+            List<int> results = pipeline.Process(numbers);
+
+            // Assert
+            Assert.Equal(new List<int> { -1, 1, 3, 5, 7 }, results);
+        }
+    }
+}
