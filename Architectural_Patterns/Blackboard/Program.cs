6d6b8ade898305c3b1afadcf48af6dc4a28bfa46
Bohdan Shaposhnyk <bohdan.shaposhnyk@extendaretail.com>
2023-06-23 20:01:05 +0300

    Add blackboard pattern

---
+++ Architectural_Patterns/Blackboard/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlackboardPattern
+{
+    // Knowledge Source: Represents a module that contributes knowledge to the Blackboard
+    public class KnowledgeSource
+    {
+        public void ProcessData(Blackboard blackboard)
+        {
+            // Access the data on the blackboard and perform some processing
+            string data = blackboard.GetData();
+            string processedData = $"Processed: {data}";
+
+            // Update the blackboard with the processed data
+            blackboard.SetData(processedData);
+
+            Console.WriteLine("Knowledge Source processed data.");
+        }
+    }
+
+    // Blackboard: Central repository that holds the shared knowledge
+    public class Blackboard
+    {
+        private string data;
+
+        public void SetData(string newData)
+        {
+            data = newData;
+        }
+
+        public string GetData()
+        {
+            return data;
+        }
+    }
+
+    // Controller: Orchestrates the interaction between knowledge sources and the blackboard
+    public class Controller
+    {
+        private List<KnowledgeSource> knowledgeSources;
+        private Blackboard blackboard;
+
+        public Controller()
+        {
+            knowledgeSources = new List<KnowledgeSource>();
+            blackboard = new Blackboard();
+        }
+
+        public void RegisterKnowledgeSource(KnowledgeSource knowledgeSource)
+        {
+            knowledgeSources.Add(knowledgeSource);
+        }
+
+        public void Run()
+        {
+            foreach (var knowledgeSource in knowledgeSources)
+            {
+                knowledgeSource.ProcessData(blackboard);
+            }
+
+            // Access the final processed data from the blackboard
+            string finalData = blackboard.GetData();
+            Console.WriteLine($"Final Data: {finalData}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the controller and knowledge sources
+            var controller = new Controller();
+            var knowledgeSource1 = new KnowledgeSource();
+            var knowledgeSource2 = new KnowledgeSource();
+
+            // Register the knowledge sources with the controller
+            controller.RegisterKnowledgeSource(knowledgeSource1);
+            controller.RegisterKnowledgeSource(knowledgeSource2);
+
+            // Run the controller to initiate knowledge processing
+            controller.Run();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Architectural_Patterns/Blackboard/Tests.cs
@@ -0,0 +1,56 @@
+﻿using BlackboardPattern;
+using Xunit;
+
+namespace BlackboardPattern.Tests
+{
+    public class KnowledgeSourceTests
+    {
+        [Fact]
+        public void ProcessData_UpdatesBlackboardWithData()
+        {
+            // Arrange
+            var knowledgeSource = new KnowledgeSource();
+            var blackboard = new Blackboard();
+            blackboard.SetData("Initial Data");
+
+            // Act
+            knowledgeSource.ProcessData(blackboard);
+
+            // Assert
+            // Verify that the blackboard data is correctly updated after processing
+            Assert.Equal("Processed: Initial Data", blackboard.GetData());
+        }
+    }
+
+    public class BlackboardTests
+    {
+        [Fact]
+        public void SetData_UpdatesBlackboardWithData()
+        {
+            // Arrange
+            var blackboard = new Blackboard();
+
+            // Act
+            blackboard.SetData("New Data");
+
+            // Assert
+            // Verify that the blackboard data is correctly set
+            Assert.Equal("New Data", blackboard.GetData());
+        }
+
+        [Fact]
+        public void GetData_ReturnsBlackboardData()
+        {
+            // Arrange
+            var blackboard = new Blackboard();
+            blackboard.SetData("Test Data");
+
+            // Act
+            string data = blackboard.GetData();
+
+            // Assert
+            // Verify that the correct data is returned from the blackboard
+            Assert.Equal("Test Data", data);
+        }
+    }
+}
