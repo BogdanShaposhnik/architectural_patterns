ba9282eb73aa55c81c04ac06383ab1ff96f7937b
Bohdan Shaposhnyk <bohdan.shaposhnyk@extendaretail.com>
2023-06-23 18:12:26 +0300

    Add Layered Architecture pattern example

---
+++ Architectural_Patterns/Layered_Architecture/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LayeredArchitectureExample
+{
+    // Data Model
+    public class DataModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    // Data Access Layer
+    public class DataAccess
+    {
+        private List<DataModel> _data;
+
+        public DataAccess()
+        {
+            _data = new List<DataModel>
+            {
+                new DataModel { Id = 1, Name = "Data 1" },
+                new DataModel { Id = 2, Name = "Data 2" },
+                new DataModel { Id = 3, Name = "Data 3" }
+            };
+        }
+
+        public List<DataModel> GetData()
+        {
+            // Simulated data retrieval from a data source
+            return _data;
+        }
+
+        public void SaveData(DataModel newData)
+        {
+            // Simulated data saving to a data source
+            _data.Add(newData);
+        }
+    }
+
+    // Business Logic Layer
+    public class BusinessLogic
+    {
+        private readonly DataAccess _dataAccess;
+
+        public BusinessLogic(DataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+
+        public List<DataModel> ProcessData()
+        {
+            // Perform business logic operations
+            List<DataModel> data = _dataAccess.GetData();
+            // Additional processing...
+
+            return data;
+        }
+
+        public void AddData(string name)
+        {
+            // Create a new data model
+            DataModel newData = new DataModel { Id = DateTime.Now.Millisecond, Name = name };
+
+            // Save the new data
+            _dataAccess.SaveData(newData);
+        }
+    }
+
+    // Presentation Layer
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataAccess dataAccess = new DataAccess();
+            BusinessLogic businessLogic = new BusinessLogic(dataAccess);
+
+            // Call the business logic layer
+            List<DataModel> data = businessLogic.ProcessData();
+
+            // Display the data
+            Console.WriteLine("Existing Data:");
+            foreach (DataModel item in data)
+            {
+                Console.WriteLine($"{item.Id} - {item.Name}");
+            }
+
+            Console.WriteLine("---------------------------------------");
+
+            // Prompt the user to add new data
+            Console.WriteLine("Enter new data:");
+            string newData = Console.ReadLine();
+            businessLogic.AddData(newData);
+
+            Console.WriteLine("New Data Added!");
+
+            Console.WriteLine("---------------------------------------");
+
+            // Display the updated data
+            data = businessLogic.ProcessData();
+            Console.WriteLine("Updated Data:");
+            foreach (DataModel item in data)
+            {
+                Console.WriteLine($"{item.Id} - {item.Name}");
+            }
+
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Architectural_Patterns/Layered_Architecture/Tests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace LayeredArchitectureExample.Tests
+{
+    public class BusinessLogicTests
+    {
+        [Fact]
+        public void ProcessData_ReturnsData()
+        {
+            // Arrange
+            var dataAccess = new DataAccess();
+            var businessLogic = new BusinessLogic(dataAccess);
+
+            // Act
+            List<DataModel> result = businessLogic.ProcessData();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(3, result.Count);
+        }
+
+        [Fact]
+        public void AddData_SavesNewData()
+        {
+            // Arrange
+            var dataAccess = new DataAccess();
+            var businessLogic = new BusinessLogic(dataAccess);
+            var newData = "New Data";
+
+            // Act
+            businessLogic.AddData(newData);
+            List<DataModel> result = businessLogic.ProcessData();
+
+            // Assert
+            Assert.Equal(4, result.Count);
+            Assert.Contains(result, d => d.Name == newData);
+        }
+    }
+}
