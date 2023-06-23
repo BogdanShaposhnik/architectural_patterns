0b1a1951e832a8da47d3594757a68dc51089cacb
Bohdan Shaposhnyk <bohdan.shaposhnyk@extendaretail.com>
2023-06-23 19:55:44 +0300

    Add MVC pattern

---
+++ Architectural_Patterns/MVC/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVCPattern
+{
+    // Model: Represents the data and business logic of the application
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    // View: Responsible for displaying the user interface and interacting with the user
+    public class UserView
+    {
+        public void DisplayUserDetails(string name, int age)
+        {
+            Console.WriteLine("User Details:");
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+        }
+    }
+
+    // Controller: Acts as an intermediary between the Model and the View, handles user input and updates the Model and View accordingly
+    public class UserController
+    {
+        private UserModel model;
+        private UserView view;
+
+        public UserController(UserModel model, UserView view)
+        {
+            this.model = model;
+            this.view = view;
+        }
+
+        public void SetUserDetails(string name, int age)
+        {
+            model.Name = name;
+            model.Age = age;
+        }
+
+        public void UpdateView()
+        {
+            view.DisplayUserDetails(model.Name, model.Age);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the Model, View, and Controller
+            var userModel = new UserModel();
+            var userView = new UserView();
+            var userController = new UserController(userModel, userView);
+
+            // Set the initial user details
+            userController.SetUserDetails("John Doe", 30);
+
+            // Update and display user details
+            userController.UpdateView();
+
+            // Modify the user details
+            userController.SetUserDetails("Jane Smith", 25);
+
+            // Update and display user details again
+            userController.UpdateView();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Architectural_Patterns/MVC/Tests.cs
@@ -0,0 +1,49 @@
+﻿using MVCPattern;
+using Xunit;
+
+namespace MVCPattern.Tests
+{
+    public class UserControllerTests
+    {
+        [Fact]
+        public void UpdateView_DisplaysUserDetails()
+        {
+            // Arrange
+            var userModel = new UserModel();
+            var userView = new UserView();
+            var userController = new UserController(userModel, userView);
+            userModel.Name = "John Doe";
+            userModel.Age = 30;
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+            // Act
+            userController.UpdateView();
+
+            // Assert
+            // Verify that the expected user details are displayed
+            // We can capture the output using a StringWriter and assert against it,
+            // but for simplicity, we'll just check if the output contains the expected values
+            var output = stringWriter.ToString();
+            Assert.Contains("User Details:", output);
+            Assert.Contains("Name: John Doe", output);
+            Assert.Contains("Age: 30", output);
+        }
+
+        [Fact]
+        public void SetUserDetails_UpdatesModel()
+        {
+            // Arrange
+            var userModel = new UserModel();
+            var userView = new UserView();
+            var userController = new UserController(userModel, userView);
+
+            // Act
+            userController.SetUserDetails("Jane Smith", 25);
+
+            // Assert
+            // Verify that the model has been updated with the new user details
+            Assert.Equal("Jane Smith", userModel.Name);
+            Assert.Equal(25, userModel.Age);
+        }
+    }
+}
