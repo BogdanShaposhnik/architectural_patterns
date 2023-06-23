0c3ba9986f2a95d9ecf9cea151e73d4166e4ee8e
Bohdan Shaposhnyk <bohdan.shaposhnyk@extendaretail.com>
2023-06-23 19:22:27 +0300

    Add Peer-to-peer pattern

---
+++ Architectural_Patterns/Peer-to-peer/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PeerToPeerPattern
+{
+    class Peer
+    {
+        public string Name { get; }
+        public List<string> ReceivedMessages { get; } = new List<string>();
+        public int ReceivedMessagesCount { get; set; }
+        public string LastProcessedMessage { get; set; }
+
+        public Peer(string name)
+        {
+            Name = name;
+        }
+
+        public async Task SendMessageAsync(string message, List<Peer> peers)
+        {
+            Console.WriteLine($"[{Name}] Sending message: {message}");
+            await BroadcastMessageAsync(message, peers);
+        }
+
+        private async Task BroadcastMessageAsync(string message, List<Peer> peers)
+        {
+            Console.WriteLine($"[{Name}] Broadcasting message: {message}");
+
+            var tasks = new List<Task>();
+            foreach (var peer in peers)
+            {
+                if (peer != this)
+                {
+                    tasks.Add(peer.ReceiveMessageAsync(message));
+                }
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        public async Task ReceiveMessageAsync(string message)
+        {
+            Console.WriteLine($"[{Name}] Received message: {message}");
+            ReceivedMessages.Add(message);
+            await ProcessMessageAsync(message);
+        }
+
+        private async Task ProcessMessageAsync(string message)
+        {
+            ReceivedMessagesCount++;
+            LastProcessedMessage = message;
+            Console.WriteLine($"[{Name}] Finished processing message: {message}");
+        }
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Peer-to-Peer Pattern Example");
+
+            var peer1 = new Peer("Peer 1");
+            var peer2 = new Peer("Peer 2");
+            var peer3 = new Peer("Peer 3");
+            var peers = new List<Peer> { peer1, peer2, peer3 };
+
+            await peer1.SendMessageAsync("Hello, peers!", peers);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Architectural_Patterns/Peer-to-peer/Tests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PeerToPeerPattern.Tests
+{
+    public class PeerTests
+    {
+        [Fact]
+        public async Task SendMessageAsync_SendsMessageToPeers()
+        {
+            // Arrange
+            var peer1 = new Peer("Peer 1");
+            var peer2 = new Peer("Peer 2");
+            var peer3 = new Peer("Peer 3");
+
+            var peers = new List<Peer> { peer1, peer2, peer3 };
+
+            // Act
+            await peer1.SendMessageAsync("Test Message", peers);
+
+            // Assert
+            Assert.Equal(0, peer1.ReceivedMessagesCount);
+            Assert.Equal(1, peer2.ReceivedMessagesCount);
+            Assert.Equal(1, peer3.ReceivedMessagesCount);
+        }
+
+        [Fact]
+        public async Task SendMessageAsync_DoesNotSendMessageToSelf()
+        {
+            // Arrange
+            var peer1 = new Peer("Peer 1");
+            var peers = new List<Peer> { peer1 };
+
+            // Act
+            await peer1.SendMessageAsync("Test Message", peers);
+
+            // Assert
+            Assert.Equal(0, peer1.ReceivedMessagesCount);
+        }
+
+        [Fact]
+        public async Task ReceiveMessageAsync_WaitsForProcessingToFinish()
+        {
+            // Arrange
+            var peer1 = new Peer("Peer 1");
+            var peer2 = new Peer("Peer 2");
+            var peers = new List<Peer> { peer1, peer2 };
+
+            // Act
+            await peer1.SendMessageAsync("Test Message", peers);
+
+            // Assert
+            Assert.True(peer2.LastProcessedMessage.EndsWith("Test Message"));
+        }
+
+        [Fact]
+        public async Task SendMessageAsync_MultipleConcurrentMessages()
+        {
+            // Arrange
+            var peer1 = new Peer("Peer 1");
+            var peer2 = new Peer("Peer 2");
+            var peer3 = new Peer("Peer 3");
+            var peers = new List<Peer> { peer1, peer2, peer3 };
+
+            // Act
+            var tasks = new List<Task>
+            {
+                peer1.SendMessageAsync("Message 1", peers),
+                peer2.SendMessageAsync("Message 2", peers),
+                peer3.SendMessageAsync("Message 3", peers)
+            };
+
+            await Task.WhenAll(tasks);
+
+            // Assert
+            Assert.Equal(2, peer1.ReceivedMessagesCount);
+            Assert.Equal(2, peer2.ReceivedMessagesCount);
+            Assert.Equal(2, peer3.ReceivedMessagesCount);
+        }
+    }
+}
