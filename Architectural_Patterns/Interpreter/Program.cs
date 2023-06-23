a17ba6f38d901923eb2c28a6419082ffb177014f
Bohdan Shaposhnyk <bohdan.shaposhnyk@extendaretail.com>
2023-06-23 20:04:23 +0300

    Add Interpreter pattern

---
+++ Architectural_Patterns/Interpreter/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterpreterPattern
+{
+    // Abstract Expression: Defines the interface for interpreting expressions
+    public abstract class Expression
+    {
+        public abstract bool Interpret(string context);
+    }
+
+    // Terminal Expression: Represents a terminal expression
+    public class TerminalExpression : Expression
+    {
+        private readonly string expression;
+
+        public TerminalExpression(string expression)
+        {
+            this.expression = expression;
+        }
+
+        public override bool Interpret(string context)
+        {
+            return context.Contains(expression);
+        }
+    }
+
+    // Non-terminal Expression: Represents a non-terminal expression
+    public class AndExpression : Expression
+    {
+        private readonly Expression expression1;
+        private readonly Expression expression2;
+
+        public AndExpression(Expression expression1, Expression expression2)
+        {
+            this.expression1 = expression1;
+            this.expression2 = expression2;
+        }
+
+        public override bool Interpret(string context)
+        {
+            return expression1.Interpret(context) && expression2.Interpret(context);
+        }
+    }
+
+    // Non-terminal Expression: Represents a non-terminal expression
+    public class OrExpression : Expression
+    {
+        private readonly Expression expression1;
+        private readonly Expression expression2;
+
+        public OrExpression(Expression expression1, Expression expression2)
+        {
+            this.expression1 = expression1;
+            this.expression2 = expression2;
+        }
+
+        public override bool Interpret(string context)
+        {
+            return expression1.Interpret(context) || expression2.Interpret(context);
+        }
+    }
+
+    // Client: Builds the expression tree and interprets the expressions
+    public class Client
+    {
+        private Expression expression;
+
+        public Client()
+        {
+            // Build the expression tree
+            Expression terminal1 = new TerminalExpression("Hello");
+            Expression terminal2 = new TerminalExpression("World");
+            expression = new OrExpression(terminal1, terminal2);
+        }
+
+        public bool Interpret(string context)
+        {
+            // Interpret the expression with the given context
+            return expression.Interpret(context);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the client
+            var client = new Client();
+
+            // Test the interpretation
+            Console.WriteLine(client.Interpret("Hello"));          // True
+            Console.WriteLine(client.Interpret("World"));          // True
+            Console.WriteLine(client.Interpret("Hello World"));    // True
+            Console.WriteLine(client.Interpret("Hi"));             // False
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Architectural_Patterns/Interpreter/Tests.cs
@@ -0,0 +1,79 @@
+﻿using InterpreterPattern;
+using Xunit;
+
+namespace InterpreterPattern.Tests
+{
+    public class ExpressionTests
+    {
+        [Theory]
+        [InlineData("Hello", "Hello", true)]
+        [InlineData("Hello", "World", false)]
+        [InlineData("Hello World", "Hello", true)]
+        [InlineData("Hello World", "World", true)]
+        [InlineData("Hi", "Hi", true)]
+        public void TerminalExpression_Interpret_ReturnsExpectedResult(string context, string expression, bool expectedResult)
+        {
+            // Arrange
+            var terminalExpression = new TerminalExpression(expression);
+
+            // Act
+            bool result = terminalExpression.Interpret(context);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(true, true, true)]
+        [InlineData(true, false, false)]
+        [InlineData(false, true, false)]
+        [InlineData(false, false, false)]
+        public void AndExpression_Interpret_ReturnsExpectedResult(bool expression1Result, bool expression2Result, bool expectedResult)
+        {
+            // Arrange
+            var expression1 = new MockExpression(expression1Result);
+            var expression2 = new MockExpression(expression2Result);
+            var andExpression = new AndExpression(expression1, expression2);
+
+            // Act
+            bool result = andExpression.Interpret("");
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(true, true, true)]
+        [InlineData(true, false, true)]
+        [InlineData(false, true, true)]
+        [InlineData(false, false, false)]
+        public void OrExpression_Interpret_ReturnsExpectedResult(bool expression1Result, bool expression2Result, bool expectedResult)
+        {
+            // Arrange
+            var expression1 = new MockExpression(expression1Result);
+            var expression2 = new MockExpression(expression2Result);
+            var orExpression = new OrExpression(expression1, expression2);
+
+            // Act
+            bool result = orExpression.Interpret("");
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        private class MockExpression : Expression
+        {
+            private readonly bool result;
+
+            public MockExpression(bool result)
+            {
+                this.result = result;
+            }
+
+            public override bool Interpret(string context)
+            {
+                return result;
+            }
+        }
+    }
+}
